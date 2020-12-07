911f7157a383385d7fd765a65ae1c74dee467c6b
Sarah Fieck <sarahtfieck@gmail.com>
2020-12-06 20:02:34 -0800

    Adds cars.sh

--- cars.sh
+++ cars.sh
@@ -8,28 +8,24 @@ echo "Type 2 to display the list of cars"
 echo "Type 3 to exit the program."
 read -r Option
 
-while :
+while [ "$Option" != 3 ]
 do
 	case $Option in
 		#first option
-		"1")
-			echo "Please enter the year of the car: "
+		"1") echo "Please enter the year of the car: "
 			read -r year
 			echo "Please enter the make of your car: "
 			read -r make
 			echo "Please enter the model of the car: "
 			read -r model
-			newcar="${year}:${make}:${model}"
-			echo "$newcar" >> My_old_cars;;
+			newCar="${year}:${make}:${model}"
+			echo "$newCar" >> My_old_cars;;
 		#second option
-		"2")
-			sort My_old_cars;;
+		"2") sort My_old_cars;;
 		#third option
-		"3") 	
-			echo "Thank you! Goodbye"
+		"3") echo "Thank you! Goodbye"
 			break;;
-	
 		*) echo "Not an option. Please input one of the following numbers."
-esac
+	esac
 done
 
